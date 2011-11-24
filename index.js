a9374b0c2ea39071a3d5c55c5f7b80f114bd1651
Dave Edelhart <dave@wonderlandlabs.com>
2011-11-24 13:31:42 -0800

    various work on loading

---
+++ index.js
@@ -0,0 +1,1 @@
+var foo = require('foo');console.log(foo(2));
--- test/simple_mvc_server/app.js
+++
@@ -1,8 +0,0 @@
-
-var Config     = require(__dirname + '/config');
-var strata_mvc = require('strata_mvc');
-var config     = new Config(__dirname);
-
-var app = strata_mvc.load_apps(config.app_path, config);
-
-module.exports = app;
